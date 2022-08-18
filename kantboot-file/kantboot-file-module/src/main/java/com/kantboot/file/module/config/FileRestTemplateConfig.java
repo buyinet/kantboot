198f1a2ecefb790ed7e70aa547c3587af3d5cc3b
fangyifeng <2453201633@qq.com>
2022-08-18 17:45:59 +0800

    update

--- kantboot-file/kantboot-file-module/src/main/java/com/kantboot/file/module/config/FileRestTemplateConfig.java
+++ kantboot-file/kantboot-file-module/src/main/java/com/kantboot/file/module/config/FileRestTemplateConfig.java
@@ -1,16 +1,16 @@
-package com.kantboot.file.module.config;
-
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class FileRestTemplateConfig {
-
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
+//package com.kantboot.file.module.config;
+//
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.client.RestTemplate;
+//
+//@Configuration
+//public class FileRestTemplateConfig {
+//
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
+//}
